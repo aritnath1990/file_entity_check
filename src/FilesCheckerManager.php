0665f01fc0ee061b7d3cb7d8cdd88ba5c4168d36
arit_nath <arit.nath@dcplkolkata.com>
2016-08-25 12:09:54 +0530

    counter correction

--- src/FilesCheckerManager.php
+++ src/FilesCheckerManager.php
@@ -6,29 +6,28 @@
  */
  
 namespace Drupal\file_checker;
- 
+
 class FilesCheckerManager {
   public function getFilesCheckerManagerValue() {
+	\Drupal::state()->set('file_checker.file_checker_status',1);
 	$q = \Drupal::entityQuery('file');
-    $r1 = $q->count()->execute();
-    $uri_count=$r1;
+    $uri_count = $q->count()->execute();
+    $loop_run=ceil($uri_count/100);
+    $x=1;
     \Drupal::state()->set('file_checker.batch_total',ceil($uri_count/100));
 	$first=1;
 	$last=100;
-	while($uri_count>$first) {
-	  $result=array();
-      $x=$first;
-      while ($x<=$last) {
-		$file=\Drupal::entityTypeManager()->getStorage('file')->load($x);
-		$result[$file->uri->value]=$file->uri->value;
-		$x=$x+1;
-      }
-      $batch = array(
+    while($loop_run>=$x)
+    {
+		$q1 = \Drupal::entityQuery('file');
+        $r2 = $q1->range(($first*$x),($last*$x))->execute();
+        $file=\Drupal::entityTypeManager()->getStorage('file')->loadMultiple($r2);
+		$batch = array(
       'title' => t('Checking File Entity Exist...'),
       'operations' => array(
         array(
           '\Drupal\file_checker\FilesCheckBatch::check',
-          array($result)
+          array($file)
           ),
         ),
       'finished' => '\Drupal\file_checker\FilesCheckBatch::entityCheckFinishedCallback',
@@ -36,7 +35,9 @@ class FilesCheckerManager {
     batch_set($batch);
     $first=$last+1;
     $last=$last+100;
+    $x=$x+1;
     }
+    
   }
  
 }
